c843622670556f5df57fd288d38378a2366d321b
panrajnold <60097161+panrajnold@users.noreply.github.com>
2025-11-04 10:12:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,13 @@
 module.exports = {
   uiPort: process.env.PORT || 1880,
   uiHost: "0.0.0.0",
-  adminAuth: null
+
+  adminAuth: {
+    type: "credentials",
+    users: [{
+      username: "admin",
+      password: "$2b$08$C7EUwVCMGHeX8Da0gN5R0uU6K/U4TiMrfpH6OZbuj1SgpnY7ecHY2",
+      permissions: "*"
+    }]
+  }
 }
